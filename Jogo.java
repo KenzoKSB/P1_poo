5b75da495c474027b1280bbe3e554f24eeb4e0a1
kenzo <rafaelkenzosatobezerra@gmail.com>
2026-04-15 13:19:50 -0300

    feat(p1): adiciona repertorio e aprendizado de musicas

---
+++ Jogo.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Jogo {
+    public static void main(String[] args) throws InterruptedException {
+
+        //cria o sujeito da classe personagem
+        Personagem p = new Personagem();
+        p.nome = "Michel";
+
+        //cria gerador para o switch case
+        Random gerador = new Random();
+
+        //cria a lista das musicas
+        ArrayList<Musica> musicas = new ArrayList<>();
+        //os elementos da lista de musica
+        musicas.add(new Musica("Jeremy"));
+        musicas.add(new Musica("Stairway to Heaven"));
+        musicas.add(new Musica("Numb"));
+        musicas.add(new Musica("Dream On"));
+        musicas.add(new Musica("Black"));
+        musicas.add(new Musica("Smells Like Teen Spirit"));
+        musicas.add(new Musica("Californication"));
+        musicas.add(new Musica("In the End"));
+        musicas.add(new Musica("Highway to Hell"));
+        musicas.add(new Musica("Fear of the Dark"));
+
+        //começa o loop
+        while(true){
+
+            //limita o gerador
+            int acao = gerador.nextInt(1, 4); 
+
+            switch(acao){
+
+                case 1:
+                    p.cacar();
+                    break;
+
+                case 2:
+                    p.comer();
+                    break;
+
+                case 3:
+                    p.dormir();
+                    break;
+            }
+
+            //independente do que ele fizer, ele vai tentar aprender uma musica
+            p.aprenderMusica(musicas);
+ 
+            //printa os aspectos e a mochila do personagem
+            System.out.println(p);
+
+            //fica verificando se o personagem t[a vivo no loop
+            if(!p.segueVivo()){
+                //se morreu, vai anunciar sua morte e mostrar seu inventario final
+                System.out.println("***O PERSONAGEM MORREU***");
+                System.out.println("Inventario final: " + p.mochila);
+                System.out.println("Repertorio final: " + p.repertorio); //novo print de morte, exibindo a lista de musicas
+                break; // encerra o loop
+            }
+
+            System.out.println("**********************");
+            Thread.sleep(5000);
+        }
+    }
+}
---
+++ Personagem.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+//cria o personagem e seus aspectos
+public class Personagem{
+    String nome;
+    private int energia;
+    private int fome;
+    private int sono;
+    ArrayList<String> mochila = new ArrayList<>();
+    ArrayList<Musica> repertorio = new ArrayList<>(); //nova array para poder guardar as musicas
+   
+    //padrão dos aspectos pra um personagem
+    Personagem(){
+        System.out.println("Construindo novo personagem");
+        energia = 10;
+        fome = 0;
+        sono = 0;
+    }
+    
+    //controla tudo da função de caçar
+    void cacar(){
+        if(energia >= 2){
+            //se ele tiver energia, ele consegue caçar e gasta 2 de energia
+            System.out.printf("%s caçando\n", nome);
+            energia -= 2;
+            
+            //em caso de sucesso, item para a mochila
+            String[] itens = {"pena", "couro", "osso", "presa", "garra"};
+            Random gerador = new Random();
+            String item = itens[gerador.nextInt(itens.length)];
+            mochila.add(item);
+        
+        }else{
+            //sem energia, não caça
+            System.out.printf("%s sem energia para caçar\n", nome);
+        }
+        
+        //tentou caçar de qualquer jeito, então fica com mais sono e fome
+        if (fome < 10) fome = fome + 1;
+        sono = sono == 10 ? sono : sono + 1;
+    }
+    
+    //controla tudo na função comer
+    void comer(){
+        if(fome >= 1){
+            //se tiver fome, come, diminui a fome e aumenta a energia
+            System.out.println(nome + " comendo");
+            energia = Math.min(energia + 1, 10);
+            fome--;
+        }
+        else{
+            //se não tiver fome, não come
+            System.out.println(nome + " sem fome");
+        }
+    }
+    
+    //controla tudo da função dormir
+    void dormir(){
+        if(sono >= 1){
+            //se ele tiver com sono, dorme, diminuindo o sono e ganhando energia
+            System.out.print(nome + " dormindo\n");
+            energia = energia == 10 ? energia : energia + 1;
+            sono -= 1;
+        }else{
+            //se não tiver sono, não dorme
+            System.out.println(nome + " sem sono");
+        }
+    }
+    
+    //controla a impressão dos status do personagem e da mochila dele, agora com a musica incluida
+    public String toString(){  
+        return String.format(
+            "%s: e:%d, f:%d, s:%d, mochila:%s, repertorio:%s",
+            nome, energia, fome, sono, mochila, repertorio
+        );
+    }
+
+    //usa o boolean pra ter uma resposta binaria pra saber se o personagem continua vivo
+    boolean vivo = true;  
+    boolean segueVivo(){
+    return energia > 0;
+    }
+
+    //controla tudo da função de aprender musica
+    void aprenderMusica(ArrayList<Musica> playlist){ 
+        Random gerador = new Random();
+        Musica m = playlist.get(gerador.nextInt(playlist.size()));
+
+        //se a musica que ele tentou aprender já estiver na lista dele, ele não aprende
+        if(repertorio.contains(m)){
+            System.out.println(nome + " ja conhece esta musica: " + m);
+        } 
+        //se não, ele aprende e acrescenta na lista
+        else {
+            repertorio.add(m);
+            System.out.println(nome + " aprendeu esta musica: " + m);
+        }
+    }
+}
